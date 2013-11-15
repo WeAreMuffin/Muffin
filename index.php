57db46a554855138dcbe8befe44edb7959979dfe
André Aubin <andre.aubin@lambdaweb.fr>
2013-11-15 16:30:19 +0100

    Ajout de contenu dans l'index
    On a rempli l'index avec les déclarations de styles, etc...
    On va aussi séparer les affichages pour plus de lisibilité, et on va donc créer un dossier "steps"
    qui contiendra toutes les étapes dans notre questionnaire.
    
    On y ajoute la première étape, step-0.php, et on y met un petit formulaire.

--- index.php
+++ index.php
@@ -9,10 +9,64 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/moon.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
+
+        <div class="header-container">
+            <header class="wrapper clearfix">
+                <h1 class="title">Trouver un titre</h1>
+                <!-- // Pas besoin de navigation pour ce site...
+                <nav>
+                    <ul>
+                        <li><a href="#">nav ul li a</a></li>
+                        <li><a href="#">nav ul li a</a></li>
+                        <li><a href="#">nav ul li a</a></li>
+                    </ul>
+                </nav>
+                -->
+            </header>
+        </div>
+
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+
+                <?php include_once 'steps/step-0.php'; ?>
+
+                <aside>
+                    <h3>aside</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
+                </aside>
+
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+        <div class="footer-container">
+            <footer class="wrapper">
+                <h3>footer</h3>
+            </footer>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+
+        <script src="js/vendor/jquery.makeforms.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src='//www.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
     </body>
 </html>
---
+++ steps/step-0.php
@@ -0,0 +1,23 @@
+<!--
+Ce fichier va contenir la primère étape du questionnaire, c'est à dire la prise
+du login de l'étudiant, l'envoi d'un code à ce dernier et la vérification de ce
+code.
+-->
+
+<article>
+    <header>
+        <aside><span class="icon-info"></span></aside>
+        <div role="title">
+            <h1>Tout d'abord, qui est-tu ?</h1>
+        </div>
+    </header>
+    <section>
+        <label for="input-login">Entre ton login pour t'identifier</label>
+        <input type="text" placeholder="Entre ton login" id="input-login" name="login">
+        <button><span class='icon-chevron-sign-right'></span></button>
+    </section>
+    <footer>
+        <h3>article footer h3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+    </footer>
+</article>
