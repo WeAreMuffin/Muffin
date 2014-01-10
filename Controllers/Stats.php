ec7a09d06d189989d3b542943858fb7f2b67960c
André Aubin <andre.aubin@lambdaweb.fr>
2014-01-10 19:38:52 +0100

    Adding stat page improvements

--- Controllers/Stats.php
+++ Controllers/Stats.php
@@ -22,7 +22,7 @@
  *
  * (c) 2013 Lambdaweb - www.lambdaweb.fr
  *
- * 
+ *
  * @author lambda2
  */
 
@@ -38,11 +38,30 @@ class Stats extends Controller
     	$u_insc_c = "SELECT DISTINCT COUNT(u.id) as c FROM c_user u";
     	$u_actif_c = "SELECT DISTINCT u.id as c FROM c_user_competences uc INNER JOIN c_user u ON uc.id_user = u.id";
 
+		$top_ten_c = "SELECT COUNT( * ) AS c, u.login AS l
+						FROM c_echanges e
+						INNER JOIN c_user u ON e.id_propose = u.id
+						GROUP BY e.id_propose
+						ORDER BY c DESC
+						LIMIT 0 , 10";
+
         $bd = Core::getBdd()->getDb();
 
         $all = 0 + $bd->query($u_all_c)->fetchObject()->c;
 		$insc = 0 + $bd->query($u_insc_c)->fetchObject()->c;
 		$actif = $bd->query($u_actif_c)->rowCount();
+		$top_ten_data = array();
+		$top_ten_logins = array();
+		$top_ten_max = 0;
+		foreach  ($bd->query($top_ten_c) as $user)
+		{
+			$top_ten_logins[] = '"'.$user['l'].'"';
+			$top_ten_data[] = $user['c'];
+			if ($user['c'] > $top_ten_max)
+				$top_ten_max = $user['c'];
+		}
+		$top_ten_logins = "[".implode(', ', $top_ten_logins).']';
+		$top_ten_data = "[".implode(', ', $top_ten_data).']';
 
         $actif = ($actif);
         $inactif = $insc - $actif;
@@ -53,6 +72,9 @@ class Stats extends Controller
         $this->addData("actif", $actif);
         $this->addData("inactif", $inactif);
         $this->addData("noninscrit", $noninscrit);
+        $this->addData("top_ten_data", $top_ten_data);
+        $this->addData("top_ten_max", $top_ten_max);
+        $this->addData("top_ten_logins", $top_ten_logins);
         $this->render ();
     }
 
